85b20cf122dc1f1bd2c89f5811903df2819a710f
Jan Ouwens <jan.ouwens@gmail.com>
2017-12-15 16:16:00 +0100

    Adds Loopy demo

---
+++ src/main/java/nl/jqno/talks/donthacktheplatform/demo1/Loopy.java
@@ -0,0 +1,17 @@
+package nl.jqno.talks.donthacktheplatform.demo1;
+
+import java.lang.reflect.Field;
+
+public class Loopy {
+    public static void main(String... args) throws Exception {
+        Integer five = 5;
+
+        Field field = Integer.class.getDeclaredField("value");
+        field.setAccessible(true);
+        field.set(five, 4);
+
+        for (Integer i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
