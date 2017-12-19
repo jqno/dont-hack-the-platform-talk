280222deb534aeba3880e0b03b8657f91acd686d
Jan Ouwens <jan.ouwens@gmail.com>
2017-12-19 08:57:06 +0100

    Improves unicode demos

--- src/main/java/nl/jqno/talks/donthacktheplatform/demo4/UnicodeEscape.java
+++ src/main/java/nl/jqno/talks/donthacktheplatform/demo4/UnicodeEscape.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class UnicodeEscape {
     @Test
     public void passing() {
-        // This test passes! \u000A if (true) return;
+        // This test passes!                                                                                                                             Move along, nothing to see here! ;-)   \u000A if (true) return;
         assertEquals("true", "false");
     }
 }
---
+++ src/main/java/nl/jqno/talks/donthacktheplatform/demo4/helper/UnicodeConverter.java
@@ -0,0 +1,31 @@
+package nl.jqno.talks.donthacktheplatform.demo4.helper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UnicodeConverter {
+    private static final String PROGRAM =
+            "public class UnicodeEscapism {\n" +
+            "    public static void main(String... args) {\n" +
+            "        System.out.println(\"Hello JVMCON!\");\n" +
+            "    }\n" +
+            "}";
+
+    public static void main(String... args) {
+        List<String> strings = PROGRAM.chars()
+                .mapToObj(UnicodeConverter::convertChar)
+                .collect(Collectors.toList());
+        String output = String.join("", strings);
+        System.out.println(strings.size());
+        System.out.println(output);
+    }
+
+    private static String convertChar(int c) {
+        if (c == 10) {
+            return "\n";
+        }
+        else {
+            return "\\u00" + Integer.toHexString(c);
+        }
+    }
+}
