0dac3c91f7f96c0f96207cdbd1d365ba77ddcdee
Jan Ouwens <jan.ouwens@gmail.com>
2017-12-20 07:17:39 +0100

    Improves Intern demo

--- src/main/java/nl/jqno/talks/donthacktheplatform/demo2/Intern.java
+++ src/main/java/nl/jqno/talks/donthacktheplatform/demo2/Intern.java
@@ -4,11 +4,12 @@ import java.lang.reflect.Field;
 
 public class Intern {
     public static void main(String... args) throws Exception {
-        System.out.println("Hello world!");
+        String s = "Hello world!";
+        System.out.println(s);
 
         Field field = String.class.getDeclaredField("value");
         field.setAccessible(true);
-        field.set("Hello world!", field.get("Hello JVMCON!"));
+        field.set(s, field.get("Hello JVMCON!"));
 
         System.out.println("Hello world!");
     }
